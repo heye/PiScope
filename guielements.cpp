d25c9df35baff4dd3642e2927e51d33c33c1008c
heye <heye.everts.1@gmail.com>
2013-02-16 09:30:42 +0000

    stuff

---
+++ guielements.cpp
@@ -0,0 +1,102 @@
+#include "guielements.h"
+
+//Fontinfo SansTypeface;
+
+
+mouse::mouse(int width, int height){
+	mWidth = width;
+	mHeight = height;
+	mX = width/2;
+	mY = height/2;
+	mDown = 0;
+}
+int mouse::getX(){
+	return mX;
+}
+int mouse::getY(){
+	return mY;
+}
+int mouse::getDown(){
+	return mDown;
+}
+void mouse::update(){
+	SDL_Event event;
+	while( SDL_PollEvent( &event ) ){                
+		switch( event.type ){
+         	case SDL_MOUSEMOTION:              
+           		mX += event.motion.xrel;
+           		mY -= event.motion.yrel;
+                		
+           		if(mX < 0) mX = 0;
+           		if(mX > mWidth) mX = mWidth;
+           		if(mY < 0 ) mY = 0;
+           		if(mY > mHeight) mY = mHeight;    
+                		
+           		break;
+          	case SDL_MOUSEBUTTONDOWN:            		
+           		mDown = 1;
+                break;
+          	case SDL_MOUSEBUTTONUP:            		
+          		mDown = 0;
+                break;
+            default:
+                break;
+        }
+    }
+}
+void mouse::draw(){
+	Stroke(255,255,255, 1);
+	if(mDown){
+		Fill(255, 255, 255, 0.5);	
+		Circle(mX, mY, 25);
+	}
+	else{
+		Fill(0, 0, 0, 0);	
+		Circle(mX, mY, 25);
+	}
+}
+
+
+
+
+Button::Button(int width, int height, int posX, int posY, int sizeX, int sizeY, const char* text){
+	mText = text;
+	mWidth = width;
+	mHeight = height;
+	mSizeX = sizeX; 
+	mSizeY = sizeY;
+	mPosX2 = posX + sizeX;
+	mPosY2 = posY + sizeY;
+	mPosX1 = posX; 
+	mPosY1 = posY;
+	mActive = 0;
+	mPressed = 0;
+}
+void Button::draw(){
+	Stroke(255,255,255, 1);
+	
+	if(mActive)
+		Fill(120, 120, 120, 1);
+	else
+		Fill(0, 0, 0, 1);
+	
+	Roundrect(mPosX1,mPosY1,mSizeX, mSizeY, 5, 5);
+	Fill(255, 100, 100, 1);
+	
+	//Fontinfo SansTypeface, SerifTypeface, MonoTypeface;
+	TextMid((mPosX1 + mSizeX/2), (mPosY1 + mSizeY/2 - 5), (char*)mText,SansTypeface, 10);
+}
+void Button::update(mouse& cursor){
+	if(mActive == 1 && !cursor.getDown())
+		mPressed = 1;
+	else
+		mPressed = 0;
+		
+	if(cursor.getDown() && cursor.getX() >= mPosX1 && cursor.getX() <= mPosX2 && cursor.getY() >= mPosY1 && cursor.getY() <= mPosY2)
+		mActive = 1;
+	else
+		mActive = 0;
+}
+int Button::getPressed(){
+	return mPressed;
+}
