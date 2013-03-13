667361ea066e0e1f464e3910acfff7b8dab68c3f
Heye Everts <heyeeverts@gmail.com>
2013-03-13 12:01:45 +0100

    VHDL: fixed spi slave device (sender)

--- PiScopeCore/PiScopeCore.srcs/sources_1/ip/fifo_generator_v9_2_0/fifo_generator_v9_2_0/simulation/fifo_generator_v9_2_0_synth.vhd
+++ PiScopeCore/PiScopeCore.srcs/sources_1/ip/fifo_generator_v9_2_0/fifo_generator_v9_2_0/simulation/fifo_generator_v9_2_0_synth.vhd
@@ -100,13 +100,13 @@ ARCHITECTURE simulation_arch OF fifo_generator_v9_2_0_synth IS
     SIGNAL rst	                          :   STD_LOGIC;
     SIGNAL wr_en                          :   STD_LOGIC;
     SIGNAL rd_en                          :   STD_LOGIC;
-    SIGNAL din                            :   STD_LOGIC_VECTOR(10-1 DOWNTO 0);
-    SIGNAL dout                           :   STD_LOGIC_VECTOR(10-1 DOWNTO 0);
+    SIGNAL din                            :   STD_LOGIC_VECTOR(11-1 DOWNTO 0);
+    SIGNAL dout                           :   STD_LOGIC_VECTOR(11-1 DOWNTO 0);
     SIGNAL full                           :   STD_LOGIC;
     SIGNAL empty                          :   STD_LOGIC;
    -- TB Signals
-    SIGNAL wr_data                        :   STD_LOGIC_VECTOR(10-1 DOWNTO 0);
-    SIGNAL dout_i                         :   STD_LOGIC_VECTOR(10-1 DOWNTO 0);
+    SIGNAL wr_data                        :   STD_LOGIC_VECTOR(11-1 DOWNTO 0);
+    SIGNAL dout_i                         :   STD_LOGIC_VECTOR(11-1 DOWNTO 0);
     SIGNAL wr_en_i                        :   STD_LOGIC := '0';
     SIGNAL rd_en_i                        :   STD_LOGIC := '0';
     SIGNAL full_i                         :   STD_LOGIC := '0';
@@ -213,8 +213,8 @@ ARCHITECTURE simulation_arch OF fifo_generator_v9_2_0_synth IS
 
     fg_dg_nv: fifo_generator_v9_2_0_dgen
       GENERIC MAP (
-          	C_DIN_WIDTH       => 10,
-		C_DOUT_WIDTH      => 10,
+          	C_DIN_WIDTH       => 11,
+		C_DOUT_WIDTH      => 11,
 		TB_SEED           => TB_SEED, 
  		C_CH_TYPE         => 0	
                  )
@@ -229,8 +229,8 @@ ARCHITECTURE simulation_arch OF fifo_generator_v9_2_0_synth IS
 
    fg_dv_nv: fifo_generator_v9_2_0_dverif
     GENERIC MAP (  
-	       C_DOUT_WIDTH       => 10,
-	       C_DIN_WIDTH        => 10,
+	       C_DOUT_WIDTH       => 11,
+	       C_DIN_WIDTH        => 11,
 	       C_USE_EMBEDDED_REG => 0,
 	       TB_SEED            => TB_SEED, 
  	       C_CH_TYPE          => 0
@@ -249,10 +249,10 @@ ARCHITECTURE simulation_arch OF fifo_generator_v9_2_0_synth IS
     GENERIC MAP ( 
               AXI_CHANNEL         => "Native",
               C_APPLICATION_TYPE  => 0,
-	      C_DOUT_WIDTH        => 10,
-	      C_DIN_WIDTH         => 10,
-	      C_WR_PNTR_WIDTH     => 14,
-    	      C_RD_PNTR_WIDTH     => 14,
+	      C_DOUT_WIDTH        => 11,
+	      C_DIN_WIDTH         => 11,
+	      C_WR_PNTR_WIDTH     => 13,
+    	      C_RD_PNTR_WIDTH     => 13,
  	      C_CH_TYPE           => 0,
               FREEZEON_ERROR      => FREEZEON_ERROR,
 	      TB_SEED             => TB_SEED, 
